19c2bffd98f07eb7e53362e6205aeebf2e70689f
0ishi <overlakehome@gmail.com>
2010-09-02 00:59:37 -0700

    add Puzzles.cs and Puzzles.csproj.

---
+++ src/Puzzles.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace oishi.com
+{
+    public class Puzzles {
+        public static bool MatchBrackets(string input)
+        {
+            Stack<char> brackets = new Stack<char> ();
+            foreach (char c in input) {
+                if ('(' == c) brackets.Push(')');
+                else if ('[' == c) brackets.Push(']');
+                else if ('{' == c) brackets.Push('}');
+                else if (')' == c || ']' == c || '}' == c) {
+                    if (0 == brackets.Count || c != brackets.Pop()) return false;
+                }
+            }
+
+            return 0 == brackets.Count;
+        }
+    }
+
+    [TestFixture]
+    public class PuzzlesTest
+    {
+        [Test]
+        public void TestMatchBrackets()
+        {
+            Assert.IsTrue(Puzzles.MatchBrackets(""));
+            Assert.IsTrue(Puzzles.MatchBrackets("()"));
+        }
+    }
+}
+
